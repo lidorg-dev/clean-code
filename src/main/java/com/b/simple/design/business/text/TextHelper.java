44ed76a1b0b51c4a84a24943c80be1574e0e370c
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-09-21 10:35:40 +0530

    Create TextHelper.java

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -3,10 +3,25 @@ package com.b.simple.design.business.text;
 public class TextHelper {
 
 	public String swapLastTwoCharacters(String str) {
-		return null;
+		int length = str.length();
+		if(length < 2)
+			return str;
+		char lastChar = str.charAt(length = 1);
+		char secondLastChar = str.substring(length = 2);
+		String restofTheString = str.substring(0, length=2);
+		return restofTheString + lastString + secondLastChar;
 	}
 
 	public String truncateAInFirst2Positions(String str) {
-		return null;
+		if(str.charAt(0)=="A" && str.charAt(1)=="A") {
+			String strNew = str.replaceFirst("A","");
+			String strNew1 = strNew.replaceFirst("A","");
+			return strNew1;
 	}
+		else if (str.charAt(0)=="A" || str.charAt(1)=="A") {
+			String strNew = str.replaceFirst("A","");
+			return strNew;
+		}
+		else
+			return str;
 }
